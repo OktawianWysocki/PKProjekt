929934193c663e96da81e1c82f8e452f1695ca26
MFrancza14 <148866697+MFrancza14@users.noreply.github.com>
2024-12-04 00:16:10 +0100

    Update klasy.cpp

--- Projektpk/Projektpk/klasy.cpp
+++ Projektpk/Projektpk/klasy.cpp
@@ -1,1 +1,55 @@
-#include"klasy.h"
+#include"KLASY.H"
+#include<iostream>
+
+
+double Sprzerzenie::uchyb() {
+    
+}
+
+double Sprzerzenie::wynikSprzerzenia() {
+    
+}
+
+double Arx::wspolczynnikA() {
+    
+}
+
+double Arx::wspolczynnikB() {
+    
+}
+
+double Arx::wspolczynnikU() {
+    
+}
+
+double Arx::wspolczynnikZ() {
+    
+}
+
+int Arx::wspolczynnikK() {
+    
+}
+
+double Arx::calosc() {
+    
+}
+
+double Pid::algortmI() {
+    
+}
+
+double Pid::algortmP() {
+    
+}
+
+double Pid::algortmD() {
+    
+}
+
+double Pid::sumaAlgorytmow() {
+    
+}
+
+void Pid::reset() {
+   
+}
