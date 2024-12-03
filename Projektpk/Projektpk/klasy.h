2afd3689ed2fbb4815c842be54ea5f9814c799d6
MFrancza14 <148866697+MFrancza14@users.noreply.github.com>
2024-12-04 00:17:13 +0100

    Update klasy.h

--- Projektpk/Projektpk/klasy.h
+++ Projektpk/Projektpk/klasy.h
@@ -1,6 +1,42 @@
 #pragma once
 #include<iostream>
 
-class Sprzerzenie{};
-class Arx{};
-class Pid{};
+class Sprzerzenie
+{
+	Arx* m_model;
+	Pid* m_pid;
+	Sygnal* m_sygnal;
+public:
+	double uchyb();
+	double wynikSprzerzenia();
+
+};
+class Arx
+{
+	double a;
+	double b;
+	int k;
+	double z = 0;
+	double y;
+public:
+	double wspolczynnikA();
+	double wspolczynnikB();
+	double wspolczynnikU();
+	double wspolczynnikZ();
+	int wspolczynnikK();
+	double calosc();
+};
+class Pid
+{
+	double kp;
+	double ti;
+	double td;
+	double poprzedniUchyb;
+public:
+	double algortmI();
+	double algortmP();
+	double algortmD();
+	double sumaAlgorytmow();
+	void reset();
+};
+class Sygnal {};
