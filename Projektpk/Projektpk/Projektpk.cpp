2ee7f8064062e94a4dc53fb8270b7d4a77394ce4
MFrancza14 <148866697+MFrancza14@users.noreply.github.com>
2024-12-03 19:47:24 +0100

    Update Projektpk.cpp

--- Projektpk/Projektpk/Projektpk.cpp
+++ Projektpk/Projektpk/Projektpk.cpp
@@ -3,19 +3,7 @@
 int main()
 {
     return 0;
-    //co tam frajer?
-    //git
-    //sadadad
-    //sdadada
-    //dadad/ad
-    //adadadad/
-    //adadaad
-    asd
-        asd
-    as
-        da
-    sd
-        aqsd
+  
     
 }
 
