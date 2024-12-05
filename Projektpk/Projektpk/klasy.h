9ef49a743a7b4ef940abfc17a68a597528716425
MFrancza14 <148866697+MFrancza14@users.noreply.github.com>
2024-12-06 00:38:49 +0100

    Update klasy.h

--- Projektpk/Projektpk/klasy.h
+++ Projektpk/Projektpk/klasy.h
@@ -1,42 +1,47 @@
-#pragma once
-#include<iostream>
+#ifndef ARX_H
+#define ARX_H
+
+#include <vector>
+
+class Arx {
+private:
+    std::vector<double> wspolczynniki_a; // Współczynniki wektora A
+    std::vector<double> wspolczynniki_b; // Współczynniki wektora B
+    int opoznienie_k; // Opóźnienie transportowe
+    std::vector<double> bufor_wejscia;  // Bufor sygnału wejściowego (u)
+    std::vector<double> bufor_wyjscia; // Bufor sygnału wyjściowego (y)
+
+public:
+    // Konstruktor klasy ARX
+    Arx(const std::vector<double>& a, const std::vector<double>& b, int k);
+
+    // Metoda wykonująca pojedynczy krok symulacji
+    double wykonaj_krok(double u);
+};
+
+#endif // ARX_H
 
 class Sprzerzenie
 {
-	Arx* m_model;
-	Pid* m_pid;
-	Sygnal* m_sygnal;
+    Arx* m_model;
+    Pid* m_pid;
+    Sygnal* m_sygnal;
 public:
-	double uchyb();
-	double wynikSprzerzenia();
+    double uchyb();
+    double wynikSprzerzenia();
 
-};
-class Arx
-{
-	double a;
-	double b;
-	int k;
-	double z = 0;
-	double y;
-public:
-	double wspolczynnikA();
-	double wspolczynnikB();
-	double wspolczynnikU();
-	double wspolczynnikZ();
-	int wspolczynnikK();
-	double calosc();
 };
 class Pid
 {
-	double kp;
-	double ti;
-	double td;
-	double poprzedniUchyb;
+    double kp;
+    double ti;
+    double td;
+    double poprzedniUchyb;
 public:
-	double algortmI();
-	double algortmP();
-	double algortmD();
-	double sumaAlgorytmow();
-	void reset();
+    double algortmI();
+    double algortmP();
+    double algortmD();
+    double sumaAlgorytmow();
+    void reset();
 };
 class Sygnal {};
